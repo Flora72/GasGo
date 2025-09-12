a228bdcb905fa19b81df5a3f205309cee4796c08
Flora72 <zahratheo569@gmail.com>
2025-09-12 09:32:24 +0300

    views defined

--- gasgoApp/views.py
+++ gasgoApp/views.py
@@ -12,3 +12,15 @@ def contact(request):
     return render(request, 'contact.html')
 def testimonials(request):
     return render(request, 'testimonials.html')
+def emergency(request):
+    return render(request, 'emergency.html')
+def track_order(request):
+    return render(request, 'track_order.html')
+def vendors(request):
+    return render(request, 'vendors.html')
+def order(request):
+    return render(request, 'order.html')
+def login(request):
+    return render(request, 'login.html')
+def signup(request):
+    return render(request, 'signup.html')
