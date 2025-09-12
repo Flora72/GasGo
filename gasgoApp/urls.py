9fd54ae11de094cab7e4cf593de866cfdcbf7f43
Flora72 <zahratheo569@gmail.com>
2025-09-12 09:35:25 +0300

    routes mapping

--- gasgoApp/urls.py
+++ gasgoApp/urls.py
@@ -8,4 +8,12 @@ urlpatterns =[
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('testimonials/', views.testimonials, name='testimonials'),
+    path('emergency/', views.emergency, name='emergency'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('track_order/', views.track_order, name='track_order'),
+    path('vendors/', views.vendors, name='vendors'),
+    path('orders/', views.order, name='orders'),
+
+
 ]
