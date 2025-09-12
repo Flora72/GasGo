4f3d255ceea6f90cadc1cd3540d0207fabbeea3b
Flora72 <zahratheo569@gmail.com>
2025-09-12 09:19:23 +0300

    definition of the views, urls, settings updates, templates and static folders updates

---
+++ gasgoApp/urls.py
@@ -0,0 +1,11 @@
+from tkinter.font import names
+
+from. import views
+from django.urls import path
+
+urlpatterns =[
+    path('',views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('testimonials/', views.testimonials, name='testimonials'),
+]
--- gasgoApp/views.py
+++ gasgoApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+# -------------------------------------
+#                   GENERAL VIEWS
+# --------------------------------------
+
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def contact(request):
+    return render(request, 'contact.html')
+def testimonials(request):
+    return render(request, 'testimonials.html')
