ae3b828f25860b1e9875a842ecebab8df865ef8d
Flora72 <zahratheo569@gmail.com>
2025-09-14 15:24:23 +0300

    gasbot logic declaration

--- gasgoApp/views.py
+++ gasgoApp/views.py
@@ -6,21 +6,35 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
 def about(request):
     return render(request, 'about.html')
+
 def contact(request):
     return render(request, 'contact.html')
+
 def testimonials(request):
     return render(request, 'testimonials.html')
+
 def emergency(request):
     return render(request, 'emergency.html')
+
 def track_order(request):
     return render(request, 'track_order.html')
+
 def vendors(request):
     return render(request, 'vendors.html')
+
 def order(request):
     return render(request, 'order.html')
+
 def login(request):
     return render(request, 'login.html')
+
 def signup(request):
     return render(request, 'signup.html')
+
+# New GasBot chatbot page
+
+def gasbot(request):
+    return render(request, 'gasbot.html')
