f5db904dee701db436b75313df884dc25db081e9
Flora72 <zahratheo569@gmail.com>
2025-09-21 12:53:16 +0300

    logout path from django auth views

--- gasgoApp/urls.py
+++ gasgoApp/urls.py
@@ -1,16 +1,17 @@
-from tkinter.font import names
-
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('testimonials/', views.testimonials, name='testimonials'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('emergency/', views.emergency, name='emergency'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout/',auth_views.LogoutView.as_view(), name='logout'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
     path('track_order/', views.track_order, name='track_order'),
     path('vendors/', views.vendors, name='vendors'),
