5e656cb1be0a73f16381fdf35ecfb1e03fde5b2c
Flora72 <zahratheo569@gmail.com>
2025-10-03 12:39:02 +0300

    Update database settings to use PostgreSQL with environment variables

--- GasGO/settings.py
+++ GasGO/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +82,15 @@ WSGI_APPLICATION = 'GasGO.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
@@ -158,4 +165,3 @@ if EMAIL_HOST_USER:
     # LOGIN_REDIRECT_URL = '/'
     # LOGOUT_REDIRECT_URL = '/'
     pass
-
