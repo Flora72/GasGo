e618153b30e8295b0af2021600e31a175506e203
Flora72 <zahratheo569@gmail.com>
2025-10-06 23:04:14 +0300

    Add payment initiation and callback routes for M-Pesa integration

--- gasgoApp/urls.py
+++ gasgoApp/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='reset_password.html'), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),  name='password_reset_complete'),
     path('payment/<str:order_id>/initiate/', views.initiate_payment, name='initiate_payment'),
+    path('payment/', views.payment, name='payment'),
     path('mpesa/callback/', views.mpesa_callback, name='mpesa_callback'),
 ]
