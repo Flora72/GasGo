287a7ff51b56a97e34b350125df1371d21aedab9
Flora72 <zahratheo569@gmail.com>
2025-10-28 22:14:47 +0300

    chore: update settings for static files, templates, and geolocation support

--- GasGO/settings.py
+++ GasGO/settings.py
@@ -30,16 +30,7 @@ SECRET_KEY = 'django-insecure-_tq8$u+xxvhe2h&7gwf&ty12zg^@(sn-8+vi!uw9$i4y8+-$=u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    'alline-hirtellous-dario.ngrok-free.dev',
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    "https://*.ngrok-free.dev",
-]
-
+ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
