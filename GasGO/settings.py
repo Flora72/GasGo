e28da988be3d8ffe5f7e116a8923942c60197d4c
Wendy-Okoth <wendyaokoth@gmail.com>
2025-10-29 13:39:31 +0300

    switch to DATABASE_URL with fallback support

--- GasGO/settings.py
+++ GasGO/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from decouple import config
 import africastalking
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,14 +81,13 @@ WSGI_APPLICATION = 'GasGO.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': dj_database_url.config(
+        default=config(
+            'DATABASE_URL',
+            default=f"postgres://{config('DB_USER')}:{config('DB_PASSWORD')}@{config('DB_HOST')}:{config('DB_PORT')}/{config('DB_NAME')}"
+        ),
+        conn_max_age=600
+    )
 }
 
 
