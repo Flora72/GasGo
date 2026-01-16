91d588d25b7c980a65d0d8713f751d05c3be75d8
Flora72 <zahratheo569@gmail.com>
2026-01-16 22:18:07 +0300

    model registering

--- gasgoApp/admin.py
+++ gasgoApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vendor, Order, USSDOrder, Profile
 
 # Register your models here.
+admin.site.register(Vendor)
+admin.site.register(Order)
+admin.site.register(USSDOrder)
+admin.site.register(Profile)
