0ef49029596f0d9a75f2a151337f4ac6211563d9
Wendy-Okoth <wendyaokoth@gmail.com>
2026-01-30 16:42:33 +0300

    Update forgot password feature

--- GasGO/settings.py
+++ GasGO/settings.py
@@ -103,13 +103,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 10
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 30
 
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = 'GasGo Assistance <gasgoassistance@gmail.com>'
-SERVER_EMAIL = DEFAULT_FROM_EMAIL
+DEFAULT_FROM_EMAIL = f'GasGo Assistance <{EMAIL_HOST_USER}>'
+SERVER_EMAIL = EMAIL_HOST_USER
 
 # Media files
 MEDIA_URL = '/media/'
