a7a4479b37fff398f34e8ae86dd659d71c349f90
Flora72 <zahratheo569@gmail.com>
2026-09-08 13:13:10 +0300

    edited the allowed hosts and debug to false for production purposes

--- GasGO/settings.py
+++ GasGO/settings.py
@@ -22,8 +22,8 @@ load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=True, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,gasgo-uby8.onrender.com').split(',')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,gasgo-uby8.onrender.com, gasgo-5otz.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
